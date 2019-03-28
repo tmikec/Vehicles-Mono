9ff9843b2b2eebec259104cd6fa522baa5eeb587
tmikec <tomislav.mikec00@gmail.com>
2019-03-28 17:25:55 +0100

    Commit

---
+++ WebApplication2/Controllers/VehiclesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        VehiclesEntities db = new VehiclesEntities();
+        
+        public ActionResult Index()
+        {
+
+            return View(db.VehicleMakes.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new VehicleMake());
+        }
+        [HttpPost]
+        public ActionResult Create(VehicleMake vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.VehicleMakes.Add(vehicle);
+              db.SaveChanges();
+                return RedirectToAction("Index");
+                    }
+            else { return View(vehicle); }
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleMake vehicle = db.VehicleMakes.Find(id);
+            if (vehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicle);
+        }
+    }
+
+}
---
+++ WebApplication2/Models/VehiclesContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class VehiclesContext : DbContext
+    {
+        public VehiclesContext() : base("name=MyDbConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WebApplication2.Models.Desc> Descript { get; set; }
+    }
+}
