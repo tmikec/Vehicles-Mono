3ca621aa82c13e2d651076c0bec7ffafea1f2548
tmikec <tomislav.mikec00@gmail.com>
2019-04-07 22:51:55 +0200

    Komentari

--- WebApplication2/Model1.Designer.cs
+++ WebApplication2/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Andrea\source\repos\WebApplication2\WebApplication2\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Andrea\Source\Repos\Vehicles-Mono2\WebApplication2\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
