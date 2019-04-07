1317e8efbb65db9bac5df6668cf948b47ad3b9db
tmikec <tomislav.mikec00@gmail.com>
2019-04-07 22:01:59 +0200

    Komentari

--- WebApplication2/Models/VehiclesContext.cs
+++ WebApplication2/Models/VehiclesContext.cs
@@ -6,12 +6,12 @@ using System.Web;
 
 namespace WebApplication2.Models
 {
-    public class VehiclesContext : DbContext
+    public class VehiclesContext : DbContext // VehiclesContext class za databazu
     {
-        public VehiclesContext() : base("name=MyDbConnection")
+        public VehiclesContext() : base("name=VehiclesContext") // Ime databaze u local serveru
         {
         }
 
-        public System.Data.Entity.DbSet<WebApplication2.Models.Desc> Descript { get; set; }
+        public System.Data.Entity.DbSet<WebApplication2.Models.Desc> Description { get; set; } // Kreira primjer DbSet
     }
 }
